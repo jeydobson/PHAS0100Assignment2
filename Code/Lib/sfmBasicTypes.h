22a5e679c579f00b9aac258387b55039d2fbf7c6
Matt Clarkson <m.clarkson@ucl.ac.uk>
2020-03-31 16:37:04 +0100

    Initial commit
    Generated from https://github.com/MattClarkson/CMakeCatch2.git.

---
+++ Code/Lib/sfmBasicTypes.h
@@ -0,0 +1,52 @@
+/*=============================================================================
+
+  PHAS0100ASSIGNMENT2: PHAS0100 Assignment 2 Social Force Model
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#ifndef sfmBasicTypes_h
+#define sfmBasicTypes_h
+
+#include "sfmWin32ExportHeader.h"
+
+/**
+* \defgroup internal internal
+* \brief Internal stuff, not for end-users.
+*/
+
+/**
+* \defgroup types types
+* \brief Package-wide data types.
+*/
+
+/**
+* \defgroup utilities utilities
+* \brief Groups of c-style functions.
+*/
+
+/**
+* \defgroup applications applications
+* \brief Small, end-user applications, most likely command line.
+*/
+
+/**
+* \file sfmBasicTypes.h
+* \brief Defines types and typedefs used in this library.
+* \ingroup types
+*/
+
+//! Single namespace for all code in this package
+namespace sfm
+{
+
+} // end namespace
+
+#endif

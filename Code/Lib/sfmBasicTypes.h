01f9b078c3099c09435d1da34b93c0911fb6d1c3
Tim Spain <51329178+timspainUCL@users.noreply.github.com>
2020-04-08 13:20:48 +0100

    Make all reference arguments const

--- Code/Lib/sfmBasicTypes.h
+++ Code/Lib/sfmBasicTypes.h
@@ -74,14 +74,14 @@ class vec2d {
   public:
     vec2d(double x, double y) 
     : xx(x), yy(y){ }
-    vec2d operator+(vec2d &b); // vector addition
-    vec2d operator-(vec2d &b); // vector subtraction
-    double operator*(vec2d &b); // scalar product
-    vec2d operator*(double &c); // scaling
-    double operator[](int i) // Get first or second component
+    vec2d operator+(const vec2d &b); // vector addition
+    vec2d operator-(const vec2d &b); // vector subtraction
+    double operator*(const vec2d &b); // scalar product
+    vec2d operator*(const double &c); // scaling
+    double operator[](int i) const// Get first or second component
     {return i ? xx : yy;}
-    double x() {return xx;} // Access the x component
-    double y() {return yy;} // Access the y component
+    double x() const {return xx;} // Access the x component
+    double y() const {return yy;} // Access the y component
 };
 
 /**
@@ -110,16 +110,16 @@ class pos2d : public vec2d {
     : vec2d(fmodulo(x, x_wrap), fmodulo(y, y_wrap)) {};
     pos2d( )  // position at the origin
     : vec2d(0., 0.) {};
-    pos2d operator+(dir2d &b);  // Move in a given direction from this position
-    dir2d operator-(pos2d &b); // direction vector from there to here a-b, 'this' is a
-    double distance(pos2d &); // distance between two positions
-    dir2d direction(pos2d &b); // direction vector of b from 'this'
-    pos2d displace(dir2d&); // position in the given direction from this initial position
+    pos2d operator+(const dir2d &b);  // Move in a given direction from this position
+    dir2d operator-(const pos2d &b); // direction vector from there to here a-b, 'this' is a
+    double distance(const pos2d &); // distance between two positions
+    dir2d direction(const pos2d &b); // direction vector of b from 'this'
+    pos2d displace(const dir2d&); // position in the given direction from this initial position
 
     static double get_x_wrap() {return x_wrap;};
     static double get_y_wrap() {return y_wrap;};
 
-    static dir2d direction(pos2d &a, pos2d &b); // direction vector from a to b
+    static dir2d direction(const pos2d &a, const pos2d &b); // direction vector from a to b
 };
 
 /**
@@ -132,24 +132,24 @@ class dir2d : public vec2d {
     dir2d(double x, double y)
     : vec2d(x, y) { };
 
-    pos2d operator+(pos2d&); // move the given position in this direction
-    dir2d operator+(dir2d&); // vector addition
-    dir2d operator-(dir2d &b); // vector subtraction
-    double operator*(dir2d &b); // Scalar product between this and b
-  	dir2d operator*(double); // Scaling vector * scalar
+    pos2d operator+(const pos2d&); // move the given position in this direction
+    dir2d operator+(const dir2d&); // vector addition
+    dir2d operator-(const dir2d &b); // vector subtraction
+    double operator*(const dir2d &b); // Scalar product between this and b
+  	dir2d operator*(const double&); // Scaling vector * scalar
     dir2d operator-(); // unary negation operator
 
-    pos2d displace(pos2d&); // Move a position in this direction
-    dir2d add(dir2d&); // vector addition
-    dir2d subtract(dir2d &b); // vector subtraction
-    double scalar_product(dir2d &b); // Scalar product between this and b
+    pos2d displace(const pos2d&); // Move a position in this direction
+    dir2d add(const dir2d&); // vector addition
+    dir2d subtract(const dir2d &b); // vector subtraction
+    double scalar_product(const dir2d &b); // Scalar product between this and b
     double length(); // length of this direction vector
-    double cos_angle(dir2d &b); // Cosine of the angle between this and b
+    double cos_angle(const dir2d &b); // Cosine of the angle between this and b
     void negate(); // negative vector, in place
     dir2d negative(); // returns the negative vector, leaving the original
 };
 
-dir2d operator*(double, dir2d); // Scaling scalar * vector
+dir2d operator*(const double&, const dir2d&); // Scaling scalar * vector
 
 } // end namespace
 
